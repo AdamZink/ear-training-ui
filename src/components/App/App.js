166b03ff07a4b39de38302a2daae34a6a428ef6e
adamzink <adamzink55@gmail.com>
2019-01-13 14:00:01 -0500

    Set up named export for Web Audio engine

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import WebAudioEngine from 'audio/WebAudioEngine';
+import { WebAudioEngine } from 'audio';
 
 export default class App extends Component {
 
