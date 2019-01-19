ee8ff4c706e2e116cbfd098d238ee1f38d316c1f
adamzink <adamzink55@gmail.com>
2019-01-19 10:26:43 -0500

    #9 - Refactor fundamental param value logic

--- src/audio/scheduler/NoteScheduler.js
+++ src/audio/scheduler/NoteScheduler.js
@@ -2,22 +2,42 @@ import { NodeConnector } from './connector';
 
 export default class NoteScheduler {
 
-  static scheduleNote(audioContext, note, paramValues) {
+  static scheduleNote(audioContext, note, inputParamValues) {
     this.currentTime = audioContext.currentTime;
 
-    this.params = {};
-
-    let fundamentalParamDefinition = note.params.filter(param => param.name === 'fundamental');
-    if (fundamentalParamDefinition.length === 1) {
-      if (paramValues.fundamental === undefined) {
-        this.params.fundamental = fundamentalParamDefinition[0].default;
-      } else {
-        this.params.fundamental = paramValues.fundamental;
-      }
-    }
+    let noteParamValues = this.getNoteParamValues(note.params, inputParamValues);
+    console.log(noteParamValues);
 
     let destination = audioContext.destination;
 
-    note.graph.forEach((node) => NodeConnector.connectNode(audioContext, node, destination, this.currentTime, this.params));
+    note.graph.forEach((node) => NodeConnector.connectNode(audioContext, node, destination, this.currentTime, noteParamValues));
+  }
+
+  static getNoteParamValues(noteParams, inputParamValues) {
+    let noteParamValues = {};
+
+    let fundamentalValue = this.getFundamentalParamValue(noteParams, inputParamValues);
+    if (fundamentalValue !== null) {
+      noteParamValues.fundamental = fundamentalValue;
+    }
+
+    return noteParamValues;
+  }
+
+  static getFundamentalParamValue(noteParams, inputParamValues) {
+    let fundamentalParams = noteParams.filter(param => param.name === 'fundamental');
+
+    if (fundamentalParams.length === 1) {
+      if (inputParamValues.fundamental === undefined) {
+        // use default fundamental value in note params definition
+        return fundamentalParams[0].default;
+      } else {
+        // use input fundamental value to override default
+        return inputParamValues.fundamental;
+      }
+    } else {
+      // fundamental param not in note params definition
+      return null;
+    }
   }
 }
