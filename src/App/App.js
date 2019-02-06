a88b4ab597077f0f5140d9939723ad9d871b855e
adamzink <adamzink55@gmail.com>
2019-02-05 23:44:17 -0500

    Add keyboard tone with exponential decay

--- src/App/App.js
+++ src/App/App.js
@@ -33,18 +33,18 @@ export default class App extends Component {
       <div className="App">
         <h2>Ear Training</h2>
         <Grid container direction="column" alignItems="center" spacing={0}>
-          <Grid item><WhiteKey variant="contained" onClick={() => WebAudioEngine.playSine(493.8833)} /></Grid>
-          <Grid item><BlackKey variant="contained" onClick={() => WebAudioEngine.playSine(466.1638)} /></Grid>
-          <Grid item><WhiteKey variant="contained" onClick={() => WebAudioEngine.playSine(440.0000)} /></Grid>
-          <Grid item><BlackKey variant="contained" onClick={() => WebAudioEngine.playSine(415.3047)} /></Grid>
-          <Grid item><WhiteKey variant="contained" onClick={() => WebAudioEngine.playSine(391.9954)} /></Grid>
-          <Grid item><BlackKey variant="contained" onClick={() => WebAudioEngine.playSine(369.9944)} /></Grid>
-          <Grid item><WhiteKey variant="contained" onClick={() => WebAudioEngine.playSine(349.2282)} /></Grid>
-          <Grid item><WhiteKey variant="contained" onClick={() => WebAudioEngine.playSine(329.6276)} /></Grid>
-          <Grid item><BlackKey variant="contained" onClick={() => WebAudioEngine.playSine(311.1270)} /></Grid>
-          <Grid item><WhiteKey variant="contained" onClick={() => WebAudioEngine.playSine(293.6648)} /></Grid>
-          <Grid item><BlackKey variant="contained" onClick={() => WebAudioEngine.playSine(277.1826)} /></Grid>
-          <Grid item><WhiteKey variant="contained" onClick={() => WebAudioEngine.playSine(261.6256)} /></Grid>
+          <Grid item><WhiteKey variant="contained" onClick={() => WebAudioEngine.playKeyboard(493.8833)} /></Grid>
+          <Grid item><BlackKey variant="contained" onClick={() => WebAudioEngine.playKeyboard(466.1638)} /></Grid>
+          <Grid item><WhiteKey variant="contained" onClick={() => WebAudioEngine.playKeyboard(440.0000)} /></Grid>
+          <Grid item><BlackKey variant="contained" onClick={() => WebAudioEngine.playKeyboard(415.3047)} /></Grid>
+          <Grid item><WhiteKey variant="contained" onClick={() => WebAudioEngine.playKeyboard(391.9954)} /></Grid>
+          <Grid item><BlackKey variant="contained" onClick={() => WebAudioEngine.playKeyboard(369.9944)} /></Grid>
+          <Grid item><WhiteKey variant="contained" onClick={() => WebAudioEngine.playKeyboard(349.2282)} /></Grid>
+          <Grid item><WhiteKey variant="contained" onClick={() => WebAudioEngine.playKeyboard(329.6276)} /></Grid>
+          <Grid item><BlackKey variant="contained" onClick={() => WebAudioEngine.playKeyboard(311.1270)} /></Grid>
+          <Grid item><WhiteKey variant="contained" onClick={() => WebAudioEngine.playKeyboard(293.6648)} /></Grid>
+          <Grid item><BlackKey variant="contained" onClick={() => WebAudioEngine.playKeyboard(277.1826)} /></Grid>
+          <Grid item><WhiteKey variant="contained" onClick={() => WebAudioEngine.playKeyboard(261.6256)} /></Grid>
         </Grid>
       </div>
     );
