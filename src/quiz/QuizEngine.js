2a89c8ccad4d10dbb05457ccccc74e2c99df6a95
adamzink <adamzink55@gmail.com>
2019-02-16 23:59:16 -0500

    Do not play the same frequency twice in a row

--- src/quiz/QuizEngine.js
+++ src/quiz/QuizEngine.js
@@ -3,8 +3,34 @@ import { keyFrequencies } from 'audio/definitions';
 
 export default class QuizEngine {
 
+  static initializeDataIfNotExists() {
+    if (this.itemPool === undefined) {
+      this.itemPool = keyFrequencies;
+      this.currentItem = undefined;
+    }
+  }
+
+  static getNextFrequency() {
+    this.initializeDataIfNotExists();
+
+    if (this.itemPool.length > 0) {
+      let selectedItemIndex = Math.floor(Math.random() * this.itemPool.length);
+      let selectedItem = this.itemPool.splice(selectedItemIndex, 1)[0];
+
+      if (this.currentItem !== undefined) {
+        this.itemPool.splice(0, 0, this.currentItem);
+      }
+      this.currentItem = selectedItem;
+
+      return selectedItem.frequency;
+    }
+    return null;
+  }
+
   static playRandomNote() {
-    let randomKeyFrequency = keyFrequencies[Math.floor(Math.random() * keyFrequencies.length)];
-    WebAudioEngine.playKeyboard(randomKeyFrequency.frequency);
+    let randomFrequency = this.getNextFrequency();
+    if (randomFrequency !== null) {
+      WebAudioEngine.playKeyboard(randomFrequency);
+    }
   }
 }
